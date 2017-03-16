ea1252d9b6bf0dd4b00c5916200da188be3fd3c1
CatDamon <dxgong1994313@163.com>
2017-03-16 11:36:18 +0800

    添加登录功能跳转到首页

--- src/main/java/cmsSystem/common/MyShiroRealm.java
+++ src/main/java/cmsSystem/common/MyShiroRealm.java
@@ -3,22 +3,33 @@ package cmsSystem.common;
 import org.apache.shiro.authc.AuthenticationException;
 import org.apache.shiro.authc.AuthenticationInfo;
 import org.apache.shiro.authc.AuthenticationToken;
+import org.apache.shiro.authc.SimpleAuthenticationInfo;
 import org.apache.shiro.authz.AuthorizationInfo;
 import org.apache.shiro.realm.AuthorizingRealm;
 import org.apache.shiro.subject.PrincipalCollection;
 
 public class MyShiroRealm extends AuthorizingRealm{
 
+	/**
+	 * 权限认证
+	 * */
 	@Override
 	protected AuthorizationInfo doGetAuthorizationInfo(PrincipalCollection principal) {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
+	/**
+	 *身份认证 
+	 * */
 	@Override
 	protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken token) throws AuthenticationException {
-		// TODO Auto-generated method stub
-		return null;
+		String userName = (String) token.getPrincipal();
+		SimpleAuthenticationInfo info = new SimpleAuthenticationInfo(userName, "1","");
+		if(info != null){
+			return info;
+		}else{
+			return null;
+		}
 	}
 
 }
--- src/main/java/cmsSystem/ctrl/login/LoginCtrl.java
+++ src/main/java/cmsSystem/ctrl/login/LoginCtrl.java
@@ -1,18 +1,26 @@
 package cmsSystem.ctrl.login;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import javax.annotation.Resource;
 import javax.security.auth.message.callback.PrivateKeyCallback.Request;
 
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authc.AuthenticationException;
+import org.apache.shiro.authc.UsernamePasswordToken;
+import org.apache.shiro.subject.Subject;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import cmsSystem.ctrl.common.BaseController;
 import cmsSystem.service.login.LoginService;
 import cmsSystem.utils.PageData;
+import cmsSystem.utils.ShiroEncriptAndDecriptUtils;
 
 @Controller
 public class LoginCtrl extends BaseController{
@@ -20,26 +28,40 @@ public class LoginCtrl extends BaseController{
 	@Resource(name="LoginServiceImpl")
 	private LoginService loginService;
 	
-	@RequestMapping(value = "/select", method=RequestMethod.GET)  
-	public ModelAndView select (){
-		logger.info("LoginCtrl select");
-		ModelAndView mv = new ModelAndView();
-		mv.setViewName("/index.html");
-		try {
-			List<PageData> list = this.loginService.select();
-			System.out.println(list);
-		} catch (Exception e) {	
-			e.printStackTrace();
-		}
-		return mv;
-	}
-	//登录
+	
+	//跳转到登录页面
 	@RequestMapping(value="/login")
 	public ModelAndView login (){
-		logger.info("LoginCtrl login...");
 		ModelAndView mv = new ModelAndView("/login.html");
-		
-		System.out.println(this.getRequest().getPathInfo());
 		return mv;
 	}
+	
+	//登录
+	@RequestMapping(value="/toLogin",method=RequestMethod.POST)
+	@ResponseBody
+	public Map<String, Object> toLogin (){
+		logger.info("LoginCtrl toLogin...");
+		Map<String, Object> map = new HashMap<String, Object>();
+		PageData userData = this.getPageData();
+		String userName = userData.getString("userName");
+		String userPassword = userData.getString("password");
+		Subject subject = SecurityUtils.getSubject();
+		UsernamePasswordToken token = new UsernamePasswordToken(userName,userPassword); 
+		try { 
+			subject.login(token); 
+			map.put("msg", "success");
+		} catch (AuthenticationException e) { 
+			map.put("message", "身份验证失败");
+			logger.error(e.getMessage());
+			return map;
+		}
+		return map;
+	}
+	
+	@RequestMapping("/toIndex")
+	public ModelAndView toIndex () {
+		ModelAndView mv = new ModelAndView("/common/index.html");
+		return mv;
+	}
+	
 }
